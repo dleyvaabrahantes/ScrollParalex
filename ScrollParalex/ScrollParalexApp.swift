3ce9fa97cc545170e4637eb0e5a5fc2bd452682b
David Leyva <dleyvaabrahantes@gmail.com>
2023-12-27 16:12:41 -0500

    Initial Commit

---
+++ ScrollParalex/ScrollParalexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollParalexApp.swift
+//  ScrollParalex
+//
+//  Created by David on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollParalexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
